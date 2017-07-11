472f200d9e21a03890effc17aa5d27d08e9e38b1
SC <939949352@qq.com>
2017-07-11 20:03:23 +0800

    show processed picture

---
+++ VMM/Vcode/Vcode/main.cpp
@@ -0,0 +1,10 @@
+#include<ViewModel\ViewModel.h>
+#include<Model/Model.h>
+#include<QtGui\qbitmap.h>
+int main(int argc, char *argv[])
+{
+
+	
+	shared_ptr<Model> pm(new Model);
+	shared_ptr<ViewModel> pvm(new ViewModel());
+}
