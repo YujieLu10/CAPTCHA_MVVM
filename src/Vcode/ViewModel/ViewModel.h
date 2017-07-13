31216244e72a1f6b08ae7c09bdf07e9c2a86e229
SC <939949352@qq.com>
2017-07-13 14:30:11 +0800

    refactor code format

--- src/Vcode/ViewModel/ViewModel.h
+++ src/Vcode/ViewModel/ViewModel.h
@@ -7,8 +7,7 @@
 #include<ViewModel/Commands/SaveFileCommand.h>
 #include<QImage>
 #include<fstream>
-class ViewModel:public Observable,public Observer
-{
+class ViewModel :public Observable, public Observer {
 private:
 	shared_ptr<QImage> pImg;
 	shared_ptr<QImage> pGrayImg;
@@ -25,7 +24,7 @@ private:
 
 	shared_ptr<Model> model;
 public:
-	
+
 	shared_ptr<BaseCommand> getLoadPictureCommand() {
 		return loadPictureCommand;
 	}
@@ -40,11 +39,11 @@ public:
 	}
 
 	ViewModel() {
-		loadPictureCommand =static_pointer_cast<BaseCommand,LoadPictureCommand>(shared_ptr<LoadPictureCommand>(new LoadPictureCommand(this)));
+		loadPictureCommand = static_pointer_cast<BaseCommand, LoadPictureCommand>(shared_ptr<LoadPictureCommand>(new LoadPictureCommand(this)));
 		processPictureCommand = static_pointer_cast<BaseCommand, ProcessPictureCommand>(shared_ptr<ProcessPictureCommand>(new ProcessPictureCommand(this)));
 		solvePictureCommand = static_pointer_cast<BaseCommand, SolvePictureCommand>(shared_ptr<SolvePictureCommand>(new SolvePictureCommand(this)));
 		saveFileCommand = static_pointer_cast<BaseCommand, SaveFileCommand>(shared_ptr<SaveFileCommand>(new SaveFileCommand(this)));
-		
+
 
 		pImg = shared_ptr<QImage>(new QImage());
 		pGrayImg = shared_ptr<QImage>(new QImage());
@@ -55,7 +54,7 @@ public:
 		errorMessage = new QString();
 	}
 	~ViewModel() {
-		
+
 	}
 	void setModel(shared_ptr<Model>& m) {
 		model = m;
@@ -64,7 +63,7 @@ public:
 		model->loadPicture(path);
 	}
 	void processPicture(int grayType, int removet, int binaryt, int denoiser) {
-		model->processPicture(grayType,removet,binaryt,denoiser);
+		model->processPicture(grayType, removet, binaryt, denoiser);
 	}
 	void solvePicture() {
 		model->solvePicture();
@@ -72,32 +71,41 @@ public:
 	void saveFile(string savePath) {
 		model->saveResult(savePath);
 	}
-	shared_ptr<QImage> getpImg() { return pImg; }
-	shared_ptr<QImage> getpGrayImg() { return pGrayImg; }
-	shared_ptr<QImage> getpRemoveBGImg() { return pRemoveBGImg; }
-	shared_ptr<QImage> getpDenoiseImg() { return pDenoiseImg; }
-	shared_ptr<QImage> getpBinaryImg() { return pBinaryImg; }
-	QString* getRes() { return res; }
-	QString* getErrorMessage() { return errorMessage; }
-	QImage cvMatToQImage(const cv::Mat &inMat)
-	{
-		switch (inMat.type())
-		{
+	shared_ptr<QImage> getpImg() {
+		return pImg;
+	}
+	shared_ptr<QImage> getpGrayImg() {
+		return pGrayImg;
+	}
+	shared_ptr<QImage> getpRemoveBGImg() {
+		return pRemoveBGImg;
+	}
+	shared_ptr<QImage> getpDenoiseImg() {
+		return pDenoiseImg;
+	}
+	shared_ptr<QImage> getpBinaryImg() {
+		return pBinaryImg;
+	}
+	QString* getRes() {
+		return res;
+	}
+	QString* getErrorMessage() {
+		return errorMessage;
+	}
+	QImage cvMatToQImage(const cv::Mat &inMat) {
+		switch (inMat.type()) {
 			// 8-bit, 4 channel
-			case CV_8UC4:
-			{
+			case CV_8UC4: {
 				QImage image(inMat.data, inMat.cols, inMat.rows, inMat.step, QImage::Format_RGB32);
 				return image;
 			}
-			// 8-bit, 3 channel
-			case CV_8UC3:
-			{
+						  // 8-bit, 3 channel
+			case CV_8UC3: {
 				QImage image(inMat.data, inMat.cols, inMat.rows, inMat.step, QImage::Format_RGB888);
 				return image.rgbSwapped();
 			}
-			// 8-bit, 1 channel
-			case CV_8UC1:
-			{
+						  // 8-bit, 1 channel
+			case CV_8UC1: {
 				static QVector<QRgb>  sColorTable;
 				// only create our color table once
 				if (sColorTable.isEmpty())
@@ -117,7 +125,6 @@ public:
 	}
 	void update(const string& attribute) {
 		if (attribute == "image") {
-			
 			*pImg = cvMatToQImage(model->getMat());
 			if (pImg->isNull()) {
 				*errorMessage = "Mat to QImage fail!";
@@ -143,10 +150,7 @@ public:
 			}
 		}
 		else if (attribute == "result") {
-			//res->fromStdWString(model->getRes().c_str());
-			//res->QString::fromUtf8(model->getRes().c_str());
 			*res = model->getRes().c_str();
-			//res->fromStdWString(model->getWRes());
 			if (res->isEmpty()) {
 				*errorMessage = "Result is empty!";
 				notify(false);
@@ -164,4 +168,3 @@ public:
 		notify(flag);
 	}
 };
-
